8e3e4e81104c97880cdc358c253b22126241a7c9
zh <zh_space@126.com>
2018-03-25 15:19:52 +0800

    * 基于Node.js开发商品列表接口 1.基于express-generate初始化node服务 2.基于mongoose实现连接MongoDB数据库 3.基于Express实现商品列表查询接口

---
+++ service/routes/goods.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var router = express.Router();
+var mongoose = require('mongoose');
+var Goods = require('../models/goods');
+
+//链接数据库
+mongoose.connect('mongodb://127.0.0.1:27017/local');
+
+mongoose.connection.on('connected', function () {
+  console.log('MongoDB connected success');
+});
+
+mongoose.connection.on('error', function () {
+  console.log('MongoDB connected fail');
+});
+
+mongoose.connection.on('disconnected', function () {
+  console.log('MongoDB connected disconnected');
+});
+
+router.get('/', function (req, res, next) {
+  //   res.send('goods list');
+  //   return next();
+  Goods.find({}, function (err, doc) {
+    if (err) {
+      res.json({
+        status: '1',
+        msg: err.message
+      })
+    } else {
+      res.json({
+        status: '0',
+        msg: '',
+        result: {
+          count: doc.length,
+          list: doc
+        }
+      });
+    }
+
+  });
+});
+
+module.exports = router;
